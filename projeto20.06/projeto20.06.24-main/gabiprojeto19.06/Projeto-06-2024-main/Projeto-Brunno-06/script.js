58e10d17ce3ce40a2e5c3f34f3b369452b70b59b
gabrielastockchineider <105800519+gabrielastockchineider@users.noreply.github.com>
2024-06-24 11:19:53 -0300

    Add files via upload

---
+++ projeto20.06/projeto20.06.24-main/gabiprojeto19.06/Projeto-06-2024-main/Projeto-Brunno-06/script.js
@@ -0,0 +1,9 @@
+const caixaprincipal  = document.querySelector(".caixa-principal");
+const caixaperguntas  = document.querySelector(".caixa-perguntas");
+const caixaalternativas = document.querySelector(".caixa-alternativas");
+const caixaresposta  = document.querySelector(".caixa-resposta");
+const textoresultado = document.querySelector(".texto-resultado");
+
+const perguntas = {
+    enunciado "Pergunta 1"
+}
